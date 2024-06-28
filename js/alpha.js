392c9bb88cd0e2c841a5cebb567ec16daca94f70
Isaac <theicean@gmail.com>
2024-06-28 10:50:16 -0600

    more psuedo code for game, run and state updaters

--- js/alpha.js
+++ js/alpha.js
@@ -5,6 +5,7 @@ let alphaState = {
     happy: 100,
     full: 100
 }
+let filth = 0
 let maxValue = 100
 let minValue = 0
 
@@ -12,7 +13,7 @@ function validateAction(property) {
     (alphaState[property] == minValue || alphaState[property] == 100) ? false : true;
 }
 
-function living(){
+function living() {
     while (alphaState.health < 0) return true;
     return false
 }
@@ -23,9 +24,23 @@ function happyUpdate() {
 }
 
 function cleanUpdate() {
-    (Math.floor((Math.random() * maxValue) + 1))
+    if (alphaState.clean > minValue && filth >= 1 ) {
+        let stink = (Math.floor((Math.random() * filth)))
+        alphaState.clean - stink;
+    }
 }
 
 function fullUpdate() {
-    (Math.floor((Math.random() * maxValue) + 1))
+    //de increment fullness
+    if (alphaState.full > minValue ) {
+        alphaState.full--;
+    }
+}
+
+function filthUpdate() {
+    //chance to generate filth if full
+}
+
+function runGame() {
+
 }
